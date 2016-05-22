a94097c7ccac0ee9f9363783df7bd2b6826dca15
devarchi33 <devarchi33@gmail.com>
2016-05-22 18:49:00 +0900

    inti project.

---
+++ src/main/java/com/cochil/CochilInputDataApplication.java
@@ -0,0 +1,12 @@
+package com.cochil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CochilInputDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CochilInputDataApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cochil/CochilInputDataApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cochil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CochilInputDataApplication.class)
+@WebAppConfiguration
+public class CochilInputDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
